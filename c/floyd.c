ee6b369660163cce8b24b3d36744539f1b8d7752
Amanskywalker <ak47su30ac@gmail.com>
2016-04-03 23:59:31 +0530

    Floyd's method added  	modified:   c/floyd.c

--- c/floyd.c
+++ c/floyd.c
@@ -11,16 +11,40 @@
 
  void floyd(int a[10][10],int n)
  {
-   int thread_id, i, j, k, s, e;
+   int thread_id, i, j, k;
+   float s, e;
    s=omp_get_wtime();
    omp_set_num_threads(2);
-   #pragma omp parallel shared(a) private(i, j, k)
-   for (i = 1; i < n; i++)
-    for (j = 1; j < n; j++)
-      for (k = 0; k < n; k++)
+   #pragma omp parallel for shared(a) private(i, j, k)
+   for (k = 0; k < n; k++)
+    for (i = 0; i < n; i++)
+      for (j = 0; j < n; j++)
       {
         thread_id=omp_get_thread_num();
         a[i][j]=min(a[i][j],a[i][k]+a[k][j]);
         printf("Thread %d : a[%d][%d] = %d\n", thread_id, i, j, a[i][j]);
       }
+    e=omp_get_wtime();
+    printf("Shortest path matrix is\n");
+    for (i = 0; i < n; i++)
+    {
+      for (j = 0; j < n; j++)
+        printf("%d\t",a[i][j]);
+      printf("\n");
+    }
+    printf("Time taken %f\n",(e-s) );
  }
+
+int main(int argc, char const *argv[])
+{
+  int size,a[10][10],i,j;
+  printf("Enter the size\n");
+  scanf("%d\n",&size);
+  printf("Enter the Elements\n");
+  for (i = 0; i < size; i++)
+    for (j = 0; j < size; j++)
+      scanf("%d",&a[i][j]);
+  printf("\n");
+  floyd(a,size);
+  return 0;
+}
